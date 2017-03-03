ebd00adbf158bc884e12e3347b8d3cf9ebec993b
Ma, Guokai <guokai.ma@intel.com>
2017-03-03 14:39:58 +0000

    [calc] fix some bug in calc, now 'x' and 'p' no longer present * and +

--- bin/calc.py
+++ bin/calc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 # add these lines to your ~/.bashrc
 #=() {
@@ -13,23 +13,23 @@ args = ''.join(sys.argv[1:])
 #print args
 args = re.sub("\[", "(", args)
 args = re.sub("\]", ")", args)
-args = re.sub("([0-9\)\s])x([0-9\)\s])", "\g<1>*\g<2>", args)
-args = re.sub("([0-9\)\s])p([0-9\(\s])", "\g<1>+\g<2>", args)
-args = re.sub("([0-9\)\s])\^([0-9\(\s])", "\g<1>**\g<2>", args)
+#args = re.sub("([0-9\)\s])x([0-9\)\s])", "\g<1>*\g<2>", args)
+#args = re.sub("([0-9\)\s])p([0-9\(\s])", "\g<1>+\g<2>", args)
+#args = re.sub("([0-9\)\s])\^([0-9\(\s])", "\g<1>**\g<2>", args)
 args = re.sub("([0-9]+)!", "factorial(\g<1>)", args)
 args = re.sub("([0-9]+)!", "factorial(\g<1>)", args)
 try:
     res = eval(args)
     if type(res) == int :
         if (0<=res<=9):
-            print res
+            print (res)
         else:
             if(res>=0):
-                print "%s = %d(0x%x)"%(args, res,res)
+                print ("%s = %d(0x%x)"%(args, res,res))
             else:
-                print "%s = %d(0x%x)"%(args, res, (1<<32)+res)
+                print ("%s = %d(0x%x)"%(args, res, (1<<32)+res))
     else:
-        print res
+        print (res)
 except:
-    print "cannot evaluate %s"%args
+    print ("cannot evaluate %s"%args)
 
