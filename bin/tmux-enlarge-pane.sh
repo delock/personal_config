59cfe07c5edefbec77fd7cfa67f75fac0c5aa105
Ma, Guokai <guokai.ma@intel.com>
2017-03-05 12:28:44 +0000

    [tmux] click on a pane will automatic enlarge pane if necessary.  Check left mouse click, right mouse click, and shift combination.

--- bin/tmux-enlarge-pane.sh
+++ bin/tmux-enlarge-pane.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 WINDOW_HEIGHT=`tmux display -p '#{window_height}'`
-tmux resize-pane -y $((WINDOW_HEIGHT*8/10))
+PANE_HEIGHT=`tmux display -p '#{pane_height}'`
+
+if [ "$1" == "" ]
+then
+    RESIZE_HEIGHT=10
+else if [ "$1" == "-l" ]
+then
+    RESIZE_HEIGHT=$2
+else
+    RESIZE_HEIGHT=$((WINDOW_HEIGHT*$1/100))
+fi
+fi
+
+if [ $RESIZE_HEIGHT -lt $PANE_HEIGHT ]
+then
+    RESIZE_HEIGHT=$PANE_HEIGHT
+fi
+
+tmux resize-pane -y $RESIZE_HEIGHT
+
