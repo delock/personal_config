22eeb0ede3e0075b864309155b20e2c27e699e18
Ma, Guokai <guokai.ma@intel.com>
2017-09-06 10:00:40 +0800

    add human readable print out

--- bin/calc.py
+++ bin/calc.py
@@ -20,16 +20,24 @@ args = re.sub("([0-9]+)!", "factorial(\g<1>)", args)
 args = re.sub("([0-9]+)!", "factorial(\g<1>)", args)
 try:
     res = eval(args)
+    if res > 1024*1024*1024 :
+        human_readable = "[" + str(int(res*100/(1024*1024*1024)+0.5)/100) + "G]"
+    elif res > 1024*1024 :
+        human_readable = "[" + str(int(res*100/(1024*1024)+0.5)/100) + "M]"
+    elif res > 1024 :
+        human_readable = "[" + str(int(res*100/1024+0.5)/100) + "K]"
+    else :
+        human_readable = ""
     if type(res) == int :
         if (0<=res<=9):
             print (res)
         else:
             if(res>=0):
-                print ("%s = %d(0x%x)"%(args, res,res))
+                print ("%s = %d(0x%x) %s"%(args, res, res, human_readable))
             else:
                 print ("%s = %d(0x%x)"%(args, res, (1<<32)+res))
     else:
-        print (res)
+        print (str(res) + human_readable)
 except:
     print ("cannot evaluate %s"%args)
 
