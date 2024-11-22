fb77352cdfc1bd34faf08b300496263b7b6c657c
Guokai Ma <guokai.ma@gmail.com>
2024-11-22 11:35:47 +0800

    update tmux config

--- bin/tmux-scripts/disk.sh
+++ bin/tmux-scripts/disk.sh
@@ -2,6 +2,9 @@ df -h|awk '
 /\/home$/ {
     print "~=" $4
 }
+/\/data$/ {
+    print "#=" $4
+}
 /\/$/ {
     print "/=" $4
 }
--- bin/tmux-status.sh
+++ bin/tmux-status.sh
@@ -3,7 +3,6 @@ echo `/home/akey/bin/tmux-scripts/cpu.sh` \
 `/home/akey/bin/tmux-scripts/temp.sh`\
 `/home/akey/bin/tmux-scripts/seperator.sh 7 0``/home/akey/bin/tmux-scripts/disk.sh`\
 `/home/akey/bin/tmux-scripts/seperator.sh 8 0``/home/akey/bin/tmux-scripts/date.sh`\
-`/home/akey/bin/tmux-scripts/seperator.sh 7 0``/home/akey/bin/tmux-scripts/syutime.sh`\
-`/home/akey/bin/tmux-scripts/seperator.sh 8 0``/home/akey/bin/tmux-scripts/seafile.sh`\
+`/home/akey/bin/tmux-scripts/seperator.sh 7 0``/home/akey/bin/tmux-scripts/seafile.sh`\
 `/home/akey/bin/tmux-scripts/seperator.sh 0 0`#[fg=red]`hostname`\
 `/home/akey/bin/tmux-scripts/seperator.sh 0 0``/home/akey/bin/tmux-scripts/todo_summary.sh`
